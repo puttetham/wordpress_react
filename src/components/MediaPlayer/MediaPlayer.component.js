a95d02ab599d5b95223309d4ad48fa09dae45b13
smuckert <magjohansson@outlook.com>
2018-03-14 17:06:48 +0100

    Mediaplayer updates

--- src/components/MediaPlayer/MediaPlayer.component.js
+++ src/components/MediaPlayer/MediaPlayer.component.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import AudioPlayer from 'react-responsive-audio-player';
 // import ReactAudioPlayer from 'react-audio-player';
+import Controls from './Controls.component';
 import './MediaPlayer.css';
 class MediaPlayer extends Component {
   constructor() {
     super();
     this.state = {
+     playing: false,
     }
   }
   
@@ -13,24 +15,34 @@ componentDidMount() {
 }
 
 render() {
-  const playlist =
-  [
+  const playlist = [
     { 
-      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
-      title: 'Track 1 by Some Artist' 
+      name: "We Were Young",
+      artist: "Odesza",
+      artwork: "https://funkadelphia.files.wordpress.com/2012/09/odesza-summers-gone-lp.jpg",
+      duration: 192,
+      src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/wwy.mp3"
     },
-    { 
-     url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
-     title: 'Some Other Artist - Track 2' 
-    }
-  ];
-
-  const controls = ['playpause', 'backskip', 'forwardskip', 'progressdisplay'];
-  
+  ]
+  // const controls = ['playpause', 'backskip', 'forwardskip', 'progressdisplay'];
+ 
 return (
       <div className="media-player">
-      <h1>hello</h1>
-      <AudioPlayer playlist={playlist} controls={controls} />
+      {/* <h1>hello</h1>
+      <AudioPlayer playlist={playlist} controls={controls} /> */}
+        <div className="Background" style={{'backgroundImage': `url(${playlist.artwork})`, 'backgroundSize': 'cover'}}>
+        </div>
+        <div className="Header">
+          <div className="Title">
+            <p>Now playing</p>
+            <p>{playlist.name}</p>
+          </div>
+        </div>
+        <audio id="audio">
+          <source src={playlist.src} type="audio/mpeg" />>
+        </audio>
+        <Controls playing={this.state.playing} />
+        
       </div>
     )
   }
