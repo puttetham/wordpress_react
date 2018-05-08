c910fb5f23e310c6509a70696a0c95acaf1b639d
puttetham <patrikhogler@hotmail.com>
2018-05-08 15:13:35 +0200

    små fix

--- src/components/Banner/Banner.component.js
+++ src/components/Banner/Banner.component.js
@@ -10,8 +10,12 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="banner"> 
-          {/* <h1 className="banner-heading">"TURN UP THE MUSIC, TURN DOWN THE DRAMA"</h1> */}
-          <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Fdanel-dantrick-temesghen%2Fnubian-galaxy-episode-1-clouds-filled-with-questions%2F" frameborder="0" ></iframe>
+          <h1 className="banner-heading">DANEL TEMESGHEN</h1>
+          <p className="banner-text">THOSE WHO DO <span className="textfix">NOT WANT TO <br  />
+             IMITATE</span> ANYTHING, <br  />
+             PRODUCE NOTHING. <br  />
+             <span className="banner-shurda">Salvador Dali</span></p>
+          {/* <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Fdanel-dantrick-temesghen%2Fnubian-galaxy-episode-1-clouds-filled-with-questions%2F" frameborder="0" ></iframe> */}
       </div>
     )
   }
--- src/components/Footer/Footer.component.js
+++ src/components/Footer/Footer.component.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 import HeadlineComponent from '../Headline/Headline.component';
+import instagram from '../../assets/insta.svg';
+import facebook from '../../assets/facebook.svg';
+import mailto from '../../assets/mailto.svg';
 import './Footer.css'
+
 
 interface Props {
 }
@@ -10,7 +14,13 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer"> 
-          <HeadlineComponent title="Footer" />
+          <p>Official website of DJ Dantrick</p>
+          <div className="logo-container">
+          <a className="contact-logo" href=""><img src={instagram} alt="" /></a>
+          <a className="contact-logo" href=""><img src={facebook} alt="" /></a>
+          <a className="contact-logo" href=""><img src={mailto} alt="" /></a>
+          </div>
+          
       </div>
     )
   }
